b93f6c61253620e817114e10765a2704689a41a7
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2016-11-30 19:30:11 +0600

    Initial Commit

---
+++ fbWarning.py
@@ -0,0 +1,41 @@
+"""
+# This is just another lazy project
+# The goal of this project is to keep a track of warned users in a Facebook Group
+# Usage: Copy profile link> 'Yes' | 'No' > Sync!
+"""
+#!python3
+
+import re
+import pyperclip
+
+pattern = re.compile(r"(?:(?:http|https):\/\/)?(?:web.)?facebook.com\/.+\?+(ref)+=(nf|ufi)")
+
+pr_data = str()
+
+
+def data_entry(filename, data):
+    """
+    This function will add the functionality to add the data to a file.
+    """
+    try:
+        with open(filename, 'a') as file:
+            file.write(data)
+            file.write('\n')
+    except IOError:
+        print("Please input a correct file name. :) ")
+
+while True:
+    raw_data = pyperclip.paste()
+    if raw_data == pr_data:
+        continue
+    if raw_data == '':
+        continue
+    else:
+        match = pattern.search(raw_data)
+        if match:
+            pr_data = match.group()
+            print("Unique ID found, Adding to file.")
+            data_entry("file.txt", pr_data)
+        else:
+            print("No FB ID Found")
+            continue
