a74eff9e3b963d4751e25d8a702c4666b78c9c5e
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2016-12-04 03:06:10 +0600

    Added a New manual and a counting mechanism with new commandline input option for flexibility

--- fbWarning.py
+++ fbWarning.py
@@ -2,11 +2,17 @@
 """
 # This is just another lazy project
 # The goal of this project is to keep a track of warned users in a Facebook Group
-# Usage: Copy profile link> 'Yes' | 'No' > Sync!
+# Usage: ./fbWarning.py [filename] -- To log profile links.
+        ./fbWarning.py count [filename] -- To count how many times a person is warned from the log file.
+
+        PS: I'll be using this for Linux Bangladesh Group for a while and find out if it's of any use
+            or just a piece of garbage.
 """
 
 import re
 import pyperclip
+import sys
+import pprint
 
 pattern = re.compile(r"(http|https):\/\/?(?:web.)?facebook.com\/.+\?+(fref)+=(nf|ufi)")
 
@@ -24,17 +30,38 @@ def data_entry(filename, data):
     except IOError:
         print("Please input a correct file name. :) ")
 
-while True:
-    raw_data = pyperclip.paste()
-    if raw_data == pr_data:
-        continue
-    if raw_data == '':
-        continue
-    else:
-        match = pattern.search(raw_data)
-        if match:
-            pr_data = match.group()
-            print("Unique ID found, Adding to file.")
-            data_entry("file.txt", pr_data)
-        else:
+
+def count(filename):
+    counter = {}
+    try:
+        with open(filename, 'r') as file:
+            for line in file.readlines():
+                counter.setdefault(line, 0)
+                counter[line] = counter[line] + 1
+    except IOError as error:
+        print("Your file is not found.\nError:{}".format(str(error)))
+    return counter
+
+
+if sys.argv[1] == 'count' and len(sys.argv) == 3:
+    number = count(sys.argv[2])
+    pprint.pprint(number)
+
+elif len(sys.argv) == 2:
+
+    while True:
+        raw_data = pyperclip.paste()
+        if raw_data == pr_data:
             continue
+        if raw_data == '':
+            continue
+        else:
+            match = pattern.search(raw_data)
+            if match:
+                pr_data = match.group()
+                print("Unique ID found, Adding to file.")
+                data_entry(sys.argv[1], pr_data)
+            else:
+                continue
+else:
+    print("Please follow the manual.")
